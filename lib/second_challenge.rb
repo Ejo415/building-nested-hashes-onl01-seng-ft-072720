87fbbca7d0d6ee66567bd4ff74436f1105fafd39
Eugene O'Neil <ejo7220@gmail.com>
2020-07-31 20:22:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,21 +1,18 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
+epic_tragedy = {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      
-      }, 
-    :capulet => {
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      
-      }
-    
-  }
+   }
+}
 
   
 end
